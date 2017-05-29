eaa83f33f175054623ea905573a30e06a5035867
Ryan Wittrup <ryan.wittrup@gmail.com>
2017-05-28 21:44:23 -0400

    Updates to app.js and create procfile to deploy to heroku

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var app = express();
-const PORT = 3000;
+// const PORT = 3000;
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
+//
+// app.listen(PORT, function() {
+//   console.log('Listening on port ' + PORT);
+// });
 
-app.listen(PORT, function() {
-  console.log('Listening on port ' + PORT);
+
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', function(request, response) {
+  response.render('public/index.html');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
